ae8ef837e2c7516305f3773905e6d1e76197cd93
60gCreatine <cphah643@gmail.com>
2025-04-01 08:01:27 +0200

    page loop and Rselerium test, needs another look

--- xG/player_ranks_webscrape.R
+++ xG/player_ranks_webscrape.R
@@ -1,11 +1,19 @@
 library(rvest)
 library(httr)
 library(tidyverse)
+library(RSelenium)
 
 # https://www.fifaindex.com/players/?gender=0&league=1&order=desc
 readRenviron("data/.Renviron")
 cookie <- Sys.getenv("cookie")
 user_agent <- Sys.getenv("agent")
+
+# get cookies with RSelenium
+rD <- rsDriver(browser = "firefox")
+remDr <- rD$client
+remDr$navigate("https://www.fifaindex.com/players/fifa24_599/?page=0&gender=0&league=1&order=desc")
+cookies <- remDr$getAllCookies()
+cookies
 
 url <-paste0("https://www.fifaindex.com/players/fifa24_599/?page=","&gender=0&league=1&order=desc")
 headers <- c(
@@ -31,44 +39,56 @@ rows <- html_elements(page, "tr[data-playerid]")
 
 players_df <- NULL
 
-player_ids <- c()
-names <- c()
-nationalities <- c()
-overalls <- c()
-potentials <- c()
-positions <- c()
-ages <- c()
-teams <- c()
+players_df <- data.frame()
 
-for (i in seq_along(rows)) {
-  row <- rows[[i]]
+for (page in 1:last_page){
+  Sys.sleep(1)
+  cat(paste0("now scraping for page: ",page, " , out of: ",last_page))
+  url_loop <- paste0("https://www.fifaindex.com/players/fifa24_599/?page=",page,"&gender=0&league=1&order=desc")
+  headers <- c(
+    "User-Agent" = user_agent,
+    "Cookie" = cookie,
+    "Accept-Language" = "en-US,en;q=0.9",
+    "Accept" = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Referer" = "https://www.fifaindex.com/",
+    "Connection" = "keep-alive"
+  )
   
-  player_ids[i] <- html_attr(row, "data-playerid")
-  names[i] <- html_text(html_element(row, 'td[data-title="Name"] a'))
-  nationalities[i] <- html_attr(html_element(row, 'td[data-title="Nationality"] a'), "title")
+  response <- GET(url, add_headers(.headers = headers))
+  response$status_code
+  page <- read_html(response)
   
-  ovr_pot <- html_elements(row, 'td[data-title="OVR / POT"] span')
-  overalls[i] <- html_text(ovr_pot[1])
-  potentials[i] <- html_text(ovr_pot[2])
+  last_page_href <- page %>%
+    html_node("a.page-link:contains('Last')") %>%
+    html_attr("href")
+  last_page <- str_extract(last_page_href, "(?<=page=)\\d+") %>% as.numeric() # only take the page number
   
-  positions[i] <- row %>%
-    html_elements('td[data-title="Preferred Positions"] a') %>%
-    html_attr("title") %>%
-    paste(collapse = ", ")
-  ages[i] <- html_text(html_element(row, 'td[data-title="Age"]'))
-  teams[i] <- row %>%
-    html_element('td[data-title="Team"] a') %>%
-    html_attr("title")
+  # get all rows (each player)
+  rows <- html_elements(page, "tr[data-playerid]")
+  
+  for (i in seq_along(rows)) {
+    row <- rows[[i]]
+    
+    player <- tryCatch({
+      data.frame(
+        player_id = html_attr(row, "data-playerid"),
+        name = html_text(html_element(row, 'td[data-title="Name"] a')),
+        nationality = html_attr(html_element(row, 'td[data-title="Nationality"] a'), "title"),
+        overall = html_text(html_elements(row, 'td[data-title="OVR / POT"] span')[1]),
+        potential = html_text(html_elements(row, 'td[data-title="OVR / POT"] span')[2]),
+        preferred_positions = paste(
+          html_attr(html_elements(row, 'td[data-title="Preferred Positions"] a'), "title"),
+          collapse = ", "
+        ),
+        age = html_text(html_element(row, 'td[data-title="Age"]')),
+        team = html_attr(html_element(row, 'td[data-title="Team"] a'), "title"),
+        stringsAsFactors = FALSE
+      )
+    }, error = function(e) {
+      message("Fejl i række ", i, ": ", e$message)
+      NULL
+    })
+    
+      players_df <- rbind(players_df, player)
+  }
 }
-
-players_df <- data.frame(
-  player_id = player_ids,
-  name = names,
-  nationality = nationalities,
-  overall = overalls,
-  potential = potentials,
-  preferred_positions = positions,
-  age = ages,
-  team = teams,
-  stringsAsFactors = FALSE
-)
