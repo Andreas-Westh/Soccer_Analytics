20e1d645059876c33b32e7cb8d861bae9a7bb3d5
60gCreatine <cphah643@gmail.com>
2025-04-09 15:20:05 +0200

    made it also scrape Team Points for xP

--- xG/team_ranking_webscrape.py
+++ xG/team_ranking_webscrape.py
@@ -5,7 +5,8 @@ import pandas as pd
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    response = page.goto("https://www.flashscore.dk/fodbold/danmark/superliga-2022-2023/tabeloversigt/#/2XDiHi8l/table/overall")
+    response = page.goto("https://www.flashscore.dk/fodbold/danmark/superliga-2023-2024/tabeloversigt/#/2XDiHi8l/table/overall")
+    #response = page.goto("https://www.flashscore.dk/fodbold/danmark/superliga-2022-2023/tabeloversigt/#/2XDiHi8l/table/overall")
     print(response.status)
     page.wait_for_selector("div.ui-table__body")  # mere præcis selector
     html = page.content()
@@ -23,7 +24,8 @@ for row in rows:
     try:
         rank = row.select_one("div.table__cell--rank div.tableCellRank").text.strip().replace(".", "")
         team = row.select_one("a.tableCellParticipant__name").text.strip()
-        data.append({"Placering": int(rank), "Hold": team})
+        points = row.select_one("span.table__cell--points").text.strip()
+        data.append({"Placering": int(rank), "Hold": team, "Point": int(points)})
     except Exception as e:
         print("Fejl:", e)
         continue
@@ -31,4 +33,5 @@ for row in rows:
 df = pd.DataFrame(data)
 print(df)
 
-df.to_csv("xG/Scraped_Data/Team_Rankings_2223.csv", index=False)
+df.to_csv("xG/Scraped_Data/Team_Rankings_2324.csv", index=False)
+#df.to_csv("xG/Scraped_Data/Team_Rankings_2223.csv", index=False)
